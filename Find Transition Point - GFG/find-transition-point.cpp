a8e6c20a2b9be367627f3c70ac5d26588814ab80
Akash Kushwaha <97276238+Akii477@users.noreply.github.com>
2023-10-29 18:51:41 +0530

    Added solution - LeetHub

---
+++ Find Transition Point - GFG/find-transition-point.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    
+    
+    int low = 0;
+    int high = n-1;
+    while( low <= high )
+    {
+        int mid = low + ( high - low )/2;
+        
+        if(arr[mid] == 1 && arr[mid-1] == 0)
+        {
+            return mid;
+        }
+        
+        else if (arr[mid] == 0 && arr[mid-1] == 0)
+        {
+            low = mid+1;
+        }
+        else
+        {
+            high = mid -1;
+        }
+    }
+    return -1;
+}
