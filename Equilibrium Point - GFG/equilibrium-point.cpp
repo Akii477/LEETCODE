476ded240d16165a94bca28bcee3abb1104d395a
Akash Kushwaha <97276238+Akii477@users.noreply.github.com>
2023-11-25 06:21:26 +0530

    Added solution - LeetHub

--- Equilibrium Point - GFG/equilibrium-point.cpp
+++ Equilibrium Point - GFG/equilibrium-point.cpp
@@ -9,78 +9,48 @@ class Solution{
     // Function to find equilibrium point in the array.
     // a: input array
     // n: size of array
-    int equilibriumPoint(long long arr[], int n) {
+    int equilibriumPoint(long long a[], int n) {
     
         // Your code here
         
-        //BRUTE FORCE:
-        
-        // if(n == 1)
-        // {
-        //     return 1;
-        // }
-        // int leftSum = 0;
-        // int rightSum = 0;
-        
-        // for(int i = 0; i<n; i++)
-        // {
-        //       leftSum =0;
-        //       for(int j = i-1; j>=0 ; j--)
-        //       {
-        //           leftSum += arr[j];
-        //       }
-               
-        //       rightSum = 0;
-        //       for(int k = i+1; k<n; k++)
-        //       {
-        //           rightSum += arr[k];
-        //       }
-               
-        //       if( leftSum == rightSum)
-        //       {
-        //           return i+1;
-        //       }
-    
-        // }
-        
-        // return -1;
+        //INITIALIZE
+        int rightSum = 0;
+        int leftSum = 0;
         
         
+        //BASE CASES 
         
-        
-        //OPTIMAL
-        
-        if(n == 1)
+        if( n == 1)
         {
             return 1;
         }
         
-        int rightSum = 0;
-        int TotalSum = 0;
-        
-        // step 1: find total sum
-        
-        for(int i =0 ; i<n; i++)
+        if( n == 2)
         {
-            TotalSum += arr[i];
+            return -1;
+        }
+        //FINDING A TOTAL SUM 
+        
+        for(int i =1; i<n; i++)
+        {
+            rightSum += a[i];
         }
         
-        // Step 2: total sum ko current element se minus karke check karenge ki wo rightSum ke equal hai ya nahi
-        for(int i = 0; i<n ; i++)
+        for(int j=0; j<n-1; j++)
         {
-            TotalSum -= arr[i];
-            
-            if(TotalSum == rightSum)
+            if(leftSum == rightSum)
             {
-                return i+1;
+                return j+1;
             }
             
-            rightSum += arr[i];
+            leftSum += a[j];
+            rightSum -= a[j+1]; // because the next element will add in leftSum 
+            
+            
         }
         return -1;
     }
 
-// TC = O(n);
 };
 
 //{ Driver Code Starts.
